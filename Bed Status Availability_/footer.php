15e86c3d4342e2739bcca048d5ce3f74e63f017f
SowatRafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-12-19 21:03:01 +0600

    New update for Bed Status

---
+++ Bed Status Availability_/footer.php
@@ -0,0 +1,12 @@
+
+                <div class="clear"></div>
+            </div>
+            <!-- // #container -->
+        </div>	
+        <!-- // #containerHolder -->
+        
+        <p id="footer">Hospital Bed Management System. &copy; 2021. All Rights Reserved - Xero_extended</p>
+    </div>
+    <!-- // #wrapper -->
+</body>
+</html>
